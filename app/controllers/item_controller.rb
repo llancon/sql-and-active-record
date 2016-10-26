de1fef0e1fc45452b685b7122ab40a2e37ed9b20
llancon <rdzalb@gmail.com>
2016-10-26 18:00:43 -0400

    Made edit page and post page, also added edit button to edit each post

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -6,5 +6,20 @@ class ItemController < ApplicationController
   def new
     @item = Item.new
   end
+  def create
+    @item = Item.new
+    @item.title = params[:item][:title]
+    @item.category = params[:item][:category]
+    @item.description = params[:item][:description]
+    @item.price = params[:item][:price]
+    @item.save
+    redirect_to root_path
+  end
+def show
+end
 
+def edit
+  @item = Item.find(params[:id])
+	render :new
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
   root 'item#index'
-	get '/items' => 'items#index' , as: :items
-  get '/items/new' => 'item#new'
- 	get 'items' => 'items#create'
+	get '/item' => 'item#index' , as: :items
+  get '/item/new' => 'item#new', as: :new_item
+  get  '/item/:id' => 'item#show', as: :item
+  post '/item' => 'item#create' #, as: :item
+  get  '/item/:id/edit' => 'item#edit', as: :edit_item
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
