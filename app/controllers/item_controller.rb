d029408170e713df45ab0947bcd065c4ef4bd75f
llancon <rdzalb@gmail.com>
2016-10-26 23:43:58 -0400

    Created update method and completed show.html.erb also added links to navigate from edit page to show page and also from show page to the home or edit page

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -3,9 +3,11 @@ class ItemController < ApplicationController
   def index
     @items = Item.all
   end
+
   def new
     @item = Item.new
   end
+
   def create
     @item = Item.new
     @item.title = params[:item][:title]
@@ -15,11 +17,24 @@ class ItemController < ApplicationController
     @item.save
     redirect_to root_path
   end
-def show
-end
 
-def edit
-  @item = Item.find(params[:id])
-	render :new
-end
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  	render :new
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.title = params[:item][:title]
+    @item.category = params[:item][:category]
+    @item.description = params[:item][:description]
+    @item.price = params[:item][:price]
+    @item.save
+    redirect_to item_path(id: @item.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
 	get '/item' => 'item#index' , as: :items
   get '/item/new' => 'item#new', as: :new_item
   get  '/item/:id' => 'item#show', as: :item
+
   post '/item' => 'item#create' #, as: :item
   get  '/item/:id/edit' => 'item#edit', as: :edit_item
+  patch '/item/:id' => 'item#update'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
